ae34c53dd2bfe2d42104e0d91b5a304fc365ec22
Hansel Castro <44871366+HanselCF07@users.noreply.github.com>
2021-05-28 16:40:35 -0500

    First Commit
    First Commit

---
+++ MVC_Formativa/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Formativa.MvcApplication" Language="C#" %>
---
+++ MVC_Formativa/Models/ViewModels/ListTablaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Formativa.Models.ViewModels
+{
+    public class ListTablaViewModel
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Correo { get; set; }
+        public DateTime Fecha_Nacimiento { get; set; }
+
+    }
+}
